123f1f1d2628be04d6479cd6f58b9c34f12bb003
VibinV <151697849+TheycallMeAI@users.noreply.github.com>
2023-12-07 20:41:41 -0500

    Update passwordGenerator-Nov-30-23.py

--- passwordGenerator-Nov-30-23.py
+++ passwordGenerator-Nov-30-23.py
@@ -1,13 +1,16 @@
 # Password Generator Created by VibinV
-# Pseudo Code : Assign the letter of alphabet to the varible char
+# Pseudo Code : Assign the letter of alphabet to the variable char
 # Ask the user what is the length of the password.
 # Choose a random letter
 # Add it to the variable password
 # Print the password
-
+import pyfiglet
 import random
 
-# Assign the ABCD's
+text = pyfiglet.figlet_format(text='VibinVk The Coder!', font='isometric1')
+print(text)
+
+# Assign the ABC's
 char = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*-=_+(/){}|:?,.;[]"
 length = int(input("Type the length of your password (integer only!!!) : "))
 password = ""
